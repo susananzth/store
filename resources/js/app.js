6553280d455ede9949c101ea3b3c6b07b720274e
Susana Piñero <susananzth@gmail.com>
2021-03-15 20:39:18 -0500

    Agregando cambios en welcome. Añadiendo estilos de bootstrap Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ require('./bootstrap');
 import { createApp, h } from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue3';
 import { InertiaProgress } from '@inertiajs/progress';
+import { BCard, BCardBody, BCardFooter, BCardHeader, BCardImg, BTable } from 'bootstrap-vue'
 
 const el = document.getElementById('app');
 
@@ -17,5 +18,11 @@ createApp({
     .mixin({ methods: { route } })
     .use(InertiaPlugin)
     .mount(el);
+    Vue.component('BCard', BCard)
+    Vue.component('BCardBody', BCardBody)
+    Vue.component('BCardFooter', BCardFooter)
+    Vue.component('BCardHeader', BCardHeader)
+    Vue.component('BCardImg', BCardImg)
+    Vue.component('BTable', BTable)
 
 InertiaProgress.init({ color: '#4B5563' });
