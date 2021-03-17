57b27eb887999f222b95b12700fffa6fbba6ed08
Susana Piñero <susananzth@gmail.com>
2021-03-16 22:02:11 -0500

    Añadiendo configuraciones en el .env y app

--- public/js/app.js
+++ public/js/app.js
@@ -22706,7 +22706,7 @@ var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("
   "class": "ml-12"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("div", {
   "class": "mt-2 text-gray-600 dark:text-gray-400 text-sm"
-}, " Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end. ")])]), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("div", {
+}, " hola ")])]), /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("div", {
   "class": "p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l"
 }, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createVNode)("div", {
   "class": "flex items-center"
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,21 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <!-- Etiquetas meta -->
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
+        <meta name="DC.Language" scheme="RFC1766" content="Spanish">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Tienda online, e-commerce con Laravel 8.12, interfaz con Vue JS 2 y bootstrap 4. Desarrollado por Susana Piñero Rodríguez @susananzth"/>
+        <meta name="keywords" content="Laravel, Laravel 8, bootstrap, bootstrap 4, e-commerce, tienda, store, roles, usuarios, admin"/>
+        <meta name="author" content="Susana Piñero Rodríguez" />
+        <meta name="copyright" content="Susana Piñero Rodríguez" />
+        <meta name="reply-to" content="susananzth@gmail.com">
+        <link REV="made" href="mailto:susananzth@gmail.com">
+        <meta http-equiv="cache-control" content="no-cache"/>
+        <meta http-equiv="expires" content="43200"/>
+        <meta name="Resource-type" content="Manual">
+        <meta name="DateCreated" content="Mon, 15 Mar 2021 11:00:00 GMT-5">
+        <meta name="Revisit-after" content="1 days">
+        <meta name="robots" content="ALL">
+        <!-- Título de la página -->
+        <title>{{ config('app.name', 'Inicio') }}</title>
+        <!-- Ícono -->
+        <link rel="icon" type="image/x-icon" href="{{asset('favicon.ico')}}" />
+        <!-- Fuentes -->
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <!-- Estilos -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+        <!-- Bootstrap-Vue 4 -->
+        <link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap/dist/css/bootstrap.min.css"/>
+        <link type="text/css" rel="stylesheet" href="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.css"/>
+        <!-- Polyfills para que soporte navegadores viejos -->
+        <script src="https://polyfill.io/v3/polyfill.min.js?features=es2015%2CIntersectionObserver"></script>
+        <!-- Mi estilo -->
+        <link rel="stylesheet" href="{{ asset('css/my-style.css') }}">
         <!-- Scripts -->
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
     </head>
     <body class="font-sans antialiased">
         @inertia
